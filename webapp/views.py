d70e0b5c4ede3b287c75e32a87bebb5557ce5858
Mir-Said <mirsaid.islamov@gmail.com>
2025-11-13 14:19:57 +0600

    Реализовал просмотр всех задач

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+from webapp.models import Task
+
+
+class TaskListView(TemplateView):
+    template_name = 'task_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = Task.objects.all()
+        return context
+
+def task_list_view(request):
+    tasks = Task.objects.all()
+    return render(request, 'task_list.html', {
+        'tasks': tasks
+    })
