516dada3fde1d8b1c63c7e178f9d757f8f15f4f6
Mir-Said <mirsaid.islamov@gmail.com>
2025-11-13 13:36:19 +0600

    Зарегистрировал модели в админке

--- webapp/admin.py
+++ webapp/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Task, Status, Type
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'description', 'status', 'type', 'created_at')
+    list_filter = ('status', 'type', 'created_at')
+    search_fields = ('title',)
+    readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Type)
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
