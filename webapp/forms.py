d3a9101816f0ecb56db641a944a7398caf04d40b
Mir-Said <mirsaid.islamov@gmail.com>
2025-11-17 15:36:41 +0600

    Добавил 2 кастомные валидации для полей формы Задач

--- webapp/forms.py
+++ webapp/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from webapp.models import Task, Status, Type
 
@@ -13,3 +14,22 @@ class TaskForm(forms.ModelForm):
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control'}),
         }
+
+    def clean_title(self):
+        title = self.cleaned_data.get('title')
+        forbidden_words = ['какие-то','запрещенные', 'запрещённые', 'слова']
+
+        if title:
+            title_lower = title.lower()
+            for word in forbidden_words:
+                if word in title_lower:
+                    raise ValidationError(
+                        f'Заголовок содержит запрещённое слово: "{word}". '
+                    )
+        return title
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description', '').strip()
+
+        if description and len(description) < 10:
+            raise ValidationError('Описание слишком короткое — минимум 10 символов.')
