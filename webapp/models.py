cdbac4ee2120f72901e0066c64597412bf3eb8ca
Mir-Said <mirsaid.islamov@gmail.com>
2025-11-24 14:03:57 +0600

    Создал абстрактную модель BaseModel

--- webapp/models.py
+++ webapp/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 
 
-class Task(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+
+    class Meta:
+        abstract = True
+
+
+class Task(BaseModel):
     title = models.CharField(max_length=100, verbose_name='Заголовок')
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
     status = models.ForeignKey('webapp.Status', on_delete=models.RESTRICT, verbose_name='Статус', related_name='tasks', blank=True, null=True)
     types = models.ManyToManyField('webapp.Type', related_name='tasks', verbose_name='Типы', blank=True)
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
 
     def __str__(self):
         return self.title
