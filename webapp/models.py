9bc2105c3116313839e5430e0b796ba44b4e9a6d
Mir-Said <mirsaid.islamov@gmail.com>
2025-11-24 16:15:22 +0600

    Создал связь между задачами и проектами, создал проект и привязал существующие задачи к нему

--- webapp/models.py
+++ webapp/models.py
@@ -25,6 +25,7 @@ class Task(BaseModel):
     description = models.TextField(verbose_name='Описание', blank=True, null=True)
     status = models.ForeignKey('webapp.Status', on_delete=models.RESTRICT, verbose_name='Статус', related_name='tasks', blank=True, null=True)
     types = models.ManyToManyField('webapp.Type', related_name='tasks', verbose_name='Типы', blank=True)
+    project = models.ForeignKey('webapp.Project', on_delete=models.SET_NULL, related_name='tasks', verbose_name='Проект',blank=True, null=True)
 
     def __str__(self):
         return self.title
